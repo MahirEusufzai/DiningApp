f0d2d179d53263859b9bf37dcf502c1ceff2c8ed
Will Gu <willgu29@gmail.com>
2014-06-25 22:34:17 -0700

    Added webView

---
+++ Bruin Dining/DiningHoursViewController.h
@@ -0,0 +1,13 @@
+//
+//  DiningHoursViewController.h
+//  Bruin Dining
+//
+//  Created by William Gu on 6/25/14.
+//  Copyright (c) 2014 Mahir Eusufzai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DiningHoursViewController : UIViewController
+
+@end
