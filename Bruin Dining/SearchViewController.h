ad5b197292c42d8f95fa6b219828fa74ebc6932f
Will Gu <willgu29@gmail.com>
2014-08-06 15:05:32 -0400

    "Added Search"

---
+++ Bruin Dining/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  Bruin Dining
+//
+//  Created by William Gu on 8/6/14.
+//  Copyright (c) 2014 Mahir Eusufzai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
